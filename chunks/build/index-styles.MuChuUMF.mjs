5541d0a557252537c0817b205a67d814bed812d1
jToki <45790991+Jtokim@users.noreply.github.com>
2026-09-20 18:19:48 +0900

    Delete chunks directory

--- chunks/build/index-styles.MuChuUMF.mjs
+++
@@ -1,9 +0,0 @@
-//#region app/pages/index.vue?vue&type=style&index=0&scoped=9c8744c6&inline&used&lang.css
-var index_vue_vue_type_style_index_0_scoped_9c8744c6_inline_used_lang_default = "ul[data-v-9c8744c6]{padding:0;font-size:14px;list-style:none}";
-
-const indexStyles_MuChuUMF = [
-  index_vue_vue_type_style_index_0_scoped_9c8744c6_inline_used_lang_default
-];
-
-export { indexStyles_MuChuUMF as default };
-//# sourceMappingURL=index-styles.MuChuUMF.mjs.map
